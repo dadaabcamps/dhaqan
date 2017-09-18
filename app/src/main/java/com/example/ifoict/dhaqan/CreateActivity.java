bf4d937bec68316d4d63d20ec2ca572e1502a96d
dadaabcamps <dadaabcamps@gmail.com>
2017-09-18 12:04:34 +0300

    implemented the functionality of Register and Login buttons in RegistrationActivity

--- app/src/main/java/com/example/ifoict/dhaqan/CreateActivity.java
+++ app/src/main/java/com/example/ifoict/dhaqan/CreateActivity.java
@@ -2,12 +2,16 @@ package com.example.ifoict.dhaqan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class CreateActivity extends AppCompatActivity {
+    Button buttonCreateStory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+        buttonCreateStory= (Button) findViewById(R.id.buttonCreateStory);
+
     }
 }
--- app/src/main/java/com/example/ifoict/dhaqan/RegistrationActivity.java
+++ app/src/main/java/com/example/ifoict/dhaqan/RegistrationActivity.java
@@ -1,13 +1,29 @@
 package com.example.ifoict.dhaqan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegistrationActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+    }
+
+    public void openLoginActivity (View view){
+        Intent i = new Intent(RegistrationActivity.this, LoginActivity.class);
+        finish();
+        startActivity(i);
+
+    }
+    public void openProfileActivity (View view){
+        Intent i = new Intent(RegistrationActivity.this, ProfileActivity.class);
+        finish();
+        startActivity(i);
+
     }
 }
